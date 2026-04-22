68ccf48c32a8950c855455bab3362df29b89679f
Vidhi098-maker <devanwaza@gmail.com>
2026-04-22 18:55:22 +0100

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,7 @@
+a = 5
+if a > 0:
+    print(a,"Is a positive number")
+
+b = 9
+if b < 8:
+    print(b,"Is a negative number")
---
+++ file3.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number :"))
+if (i < 15):
+    print("I is smaller than 15")
+    print("I'm in if block")
+else:
+    print("I is greater than 15")
+    print("I'm in else block")
+print("i'm not in if and not in else block ")
